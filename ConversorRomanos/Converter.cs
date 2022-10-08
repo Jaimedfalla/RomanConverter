8807d9b119b1faa4176fadeb567a310781605905
Jaime E Falla <65088216+Jaimedfalla@users.noreply.github.com>
2022-10-08 12:58:50 -0500

    Implementación de convertidor a números romanos

---
+++ ConversorRomanos/Converter.cs
@@ -0,0 +1,64 @@
+﻿namespace ConversorRomanos
+{
+    public static class Converter
+    {
+        private static readonly Dictionary<int, char> _romanos = new Dictionary<int, char>() {
+        {
+            1,'I'
+        },
+        {
+            5,'V'
+        },
+        {
+            10,'X'
+        },
+        {
+            50,'L'
+        },
+        {
+            100,'C'
+        },
+        {
+            500,'D'
+        },
+        {
+            1000,'M'
+        }
+    };
+
+        public static void ConvertNumber(int number, ref string converted, int potencia = 1)
+        {
+            if (number == 0) return;
+
+            double valorPotencia = Math.Pow(10, potencia - 1);
+            int cociente = (int)(number / Math.Pow(10, potencia));
+            double residuo = number % Math.Pow(10, potencia);
+
+            if (residuo != 0)
+            {
+                if (!_romanos.ContainsKey((int)residuo))
+                {
+                    var units = _romanos[(int)Math.Pow(10, potencia - 1)];
+                    var menor = _romanos.LastOrDefault(c => c.Key <= residuo && c.Key >= valorPotencia);
+                    var mayor = _romanos.FirstOrDefault(c => c.Key >= residuo && c.Key >= valorPotencia);
+
+                    int difference = (int)((mayor.Key - residuo) / valorPotencia);
+                    if (difference > 1)
+                    {
+                        difference = (int)((residuo - menor.Key) / valorPotencia);
+                        converted = $"{menor.Value}{"".PadRight(difference, units)}{converted}";
+                    }
+                    else
+                        converted = $"{units}{mayor.Value}{converted}";
+                }
+                else
+                {
+                    ; converted = $"{_romanos[(int)residuo]}{converted}";
+                }
+            }
+
+            potencia++;
+            ConvertNumber(number - (int)residuo, ref converted, potencia);
+        }
+    }
+}
---
+++ TestRomanConverter/RomanConverter.cs
@@ -0,0 +1,22 @@
+using ConversorRomanos;
+namespace TestRomanConverter
+{
+    [TestFixture]
+    public class Tests
+    {
+        [Test]
+        [TestCase(1,"I")]
+        [TestCase(5, "V")]
+        [TestCase(8, "VIII")]
+        [TestCase(80, "LXXX")]
+        [TestCase(90, "XC")]
+        [TestCase(91, "XCI")]
+        [TestCase(264, "CCLXIV")]
+        public void Test_Decimal_To_Roman_Number_Success(int number,string expected)
+        {
+            string converted = string.Empty;
+            Converter.ConvertNumber(number, ref converted);
+            Assert.That(converted, Is.EqualTo(expected));
+        }
+    }
+}
